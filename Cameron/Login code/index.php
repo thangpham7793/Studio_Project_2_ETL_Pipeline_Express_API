de169a018342f776e3c9a70ad5005a914db9f8f3
CGHill <cameron.hill2@hotmail.com>
2020-07-27 17:44:06 +1200

    Added map to main page, started on sidebar

--- Cameron/Login code/index.php
+++ Cameron/Login code/index.php
@@ -3,7 +3,12 @@
  ?>
 
  <main>
-   <!-- Content here -->
+    <div class="map-container" id="map"></div>
+    <script src="index.js"></script>
+    <div class="sidebar">
+      <input class="sidebar_input" type="text" name="" value="" placeholder="Delivery address...">
+      <input class="sidebar_input" type="text" name="" value="" placeholder="Material/SIC code...">
+    </div>
  </main>
 
  <?php
--- Cameron/Login code/signup_script.js
+++ Cameron/Login code/signup_script.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+
   $("form").submit(function(event) {
     event.preventDefault();
     var firstName = $("#inputFirstName").val();
@@ -13,7 +23,8 @@ $(document).ready(function() {
       surname: surname,
       email: email,
       pwd: pwd,
-      pwdConfirm: pwdConfirm
+      pwdConfirm: pwdConfirm,
+      signup-submit: "yes"
     });
   });
 });
