fea904e42effecda1c27cfcc155077c93984b222
Thang Pham <thangnus@gmail.com>
2020-07-29 14:15:19 +1200

    remove cached repo

--- us_mines_api/tests/unit-tests/testUtils.js
+++
@@ -1,8 +0,0 @@
-const { pathToRegexp } = require('path-to-regexp')
-
-function parsePath(pattern, input) {
-	const regexp = pathToRegexp(pattern)
-	return regexp.exec(input)
-}
-
-module.exports = parsePath
